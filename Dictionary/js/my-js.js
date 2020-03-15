d72325eb9d1a5accd91dc8901e64790bbf43e7c8
line2nine <hailinh203@gmail.com>
2020-03-15 17:23:09 +0700

    Add files

--- Dictionary/js/my-js.js
+++ Dictionary/js/my-js.js
@@ -1,11 +1,15 @@
+function findByKeyword(eng, keyword, vie) {
+    for (let i = 0; i < eng.length; i++) {
+        if (keyword === eng[i]) {
+            return vie[i];
+        }
+    }
+    return null;
+}
+
 function translateAuto() {
     let dictionaryEng = ["apple", "keyboard", "mouse", "cat", "dog", "buffalo"];
     let dictionaryVie = ["quả táo", "bàn phím", "chuột", "con mèo", "con chó", "con trâu"];
     let transFrom = document.getElementById("inputFrom").value;
-    for (let i = 0; i < dictionaryEng.length; i++) {
-        if (transFrom === dictionaryEng[i]) {
-            document.getElementById("outputTo").value = dictionaryVie[i];
-            break;
-        }
-    }
+    document.getElementById("outputTo").value = findByKeyword(dictionaryEng, transFrom, dictionaryVie);
 }
